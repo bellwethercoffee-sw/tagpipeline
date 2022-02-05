29e63ca42ca32193ef0cac06429865257bb24134
Mihai Marian <monjunkie@Mihais-MacBook-Air.local>
2022-02-05 19:45:09 +0200

    Prepare for release v6.1.2-2022-02-05

--- git_release.py
+++ git_release.py
@@ -1,47 +1,34 @@
 import fileinput
 import os
 import requests
+import re
 from git import Repo
-from datetime import datetime
-REPO_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', ''))
+import datetime
 
-
-def get_latest_release(owner: str, repo_name: str) -> str:
-    r = requests.get(
-        url=f'https://api.github.com/repos/{owner}/{repo_name}/releases/latest')
-    data = r.json()
-    return data['tag_name']
-
-
-def create_release(previous_version: str, next_version: str):
-    repo = Repo(REPO_PATH)
-    # Create & checkout new release branch
-    release_branch = repo.create_head(f'RELEASE-{next_version}')
-    release_branch.checkout()
-    # Update version in README.md
-    replace_version_string('README.md', previous_version, next_version[1:])
-    # Commit changes
-    repo.git.commit('-am', f'Prepare for release {next_version[1:]}.')
-    # Remove 'v' prefix and create tag
-    new_tag = repo.create_tag(
-        next_version[1:], message=f'Version {next_version[1:]}')
-    # Push updates
-    repo.git.push('--set-upstream', 'origin', release_branch)
-    repo.remotes.origin.push(new_tag)
-
-
-def replace_version_string(file_name: str, previous_version: str, next_version: str):
-    print(f'{file_name} updated')
-    with fileinput.FileInput(f'{REPO_PATH}/{file_name}', inplace=True) as file:
-        for line in file:
-            print(line.replace(previous_version, next_version), end='')
-
+def increment_ver(version):
+    version = version.split('.')
+    version[2] = str(int(version[2]) + 1)
+    return '.'.join(version)
 
 if __name__ == '__main__':
-    latest_action_release = get_latest_release(
-        owner='bellwethercoffee-sw', repo_name='tagpipeline')
-
-    latest_action_version = f'v{latest_action_release}'
-
-    create_release(latest_action_release, "test")
-    print('new release')
+    now = datetime.datetime.now().strftime('%Y-%m-%d')
+    print(now)
+    repo = Repo('.', search_parent_directories=True)
+    REPO_PATH = repo.working_tree_dir
+    repo = Repo(REPO_PATH)
+    tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
+    latest_tag = str(tags[-1])
+    print(latest_tag)
+    new_tag = increment_ver(str(latest_tag))
+    new_tag_date = new_tag+"-"+str(now)
+    print(new_tag_date)
+    #REPLACE VERSION IN THE FILE
+    with open('README.md', 'r') as file:
+        filedata = file.read()
+    filedata = filedata.replace(latest_tag, new_tag_date)
+    with open('file.txt', 'w') as file:
+        file.write(filedata)
+    repo.git.commit('-am', f'Prepare for release {new_tag_date}')
+    new_tag = repo.create_tag(new_tag_date, message=f'Version {new_tag_date}')
+    repo.git.push('--set-upstream', 'origin')
+    repo.remotes.origin.push(new_tag_date)
